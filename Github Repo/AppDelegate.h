523b3ab9a38596674203fcfd567531e5c6daa4f2
Ali <alidahesh@yahoo.com>
2016-11-21 14:11:57 -0800

    Initial Commit

---
+++ Github Repo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github Repo
+//
+//  Created by Ali Dahesh on 2016-11-21.
+//  Copyright © 2016 Ali Dahesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github Repo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github Repo
+//
+//  Created by Ali Dahesh on 2016-11-21.
+//  Copyright © 2016 Ali Dahesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
